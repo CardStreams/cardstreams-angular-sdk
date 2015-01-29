8734636dd7c993a3b6cfbb6f086f63903ded7b56
Lukas Nowacki <lucjan.nowacki@gmail.com>
2015-01-29 19:36:36 +0100

    Move header setting outside of the interceptor to avoid using

--- ls-angular-sdk.js
+++ ls-angular-sdk.js
@@ -4,43 +4,46 @@
   var component = angular.module("ls.lsAngularSDK", []);
 
   component
-    .factory("lsAngularSDKFactory", function($rootScope, $http, $q) {
+    .factory("lsAngularSDKFactory", function($http, $q) {
 
-      $rootScope.app_token = null;
-      $rootScope.app_id = null;
-      $rootScope.app_key = null;
-      $rootScope.api_url = null;
-      $rootScope.socket_url = null;
+      var app_token, app_id, app_key, api_url, socket_url;
 
       return {
         init: function(options) {
 
           if (options.app_token) {
-            $rootScope.app_token = options.app_token;
+            app_token = options.app_token;
           } else {
             if (!options.app_id) {
               throw new Error("LS Angular SDK: app_id is not provided.");
             } else {
-              $rootScope.app_id = options.app_id;
+              app_id = options.app_id;
             }
 
             if (!options.app_key) {
               throw new Error("LS Angular SDK: app_key is not provided.");
             } else {
-              $rootScope.app_key = options.app_key;
+              app_key = options.app_key;
             }
           }
 
-          $rootScope.api_url = options.api_url || "https://api.lifestreams.com/beta1";
-          $rootScope.socket_url = options.socket_url || "https://api.lifestreams.com:5500";
+          api_url = options.api_url || "https://api.lifestreams.com/beta1";
+          socket_url = options.socket_url || "https://api.lifestreams.com:5500";
+
+          if (app_token) {
+            $http.defaults.headers.common["Authorization"] = "bearer " + app_token
+          } else {
+            $http.defaults.headers.common["X-Lifestreams-3scale-AppId"] = app_id;
+            $http.defaults.headers.common["X-Lifestreams-3scale-AppKey"] = app_key;
+          }
         },
 
         getAllTimelines: function() {
-          return $http.get($rootScope.api_url + "/timelines");
+          return $http.get(api_url + "/timelines");
         },
 
         getTimelineInfo: function(timelineId) {
-          return $http.get($rootScope.api_url + "/timelines/" + timelineId);
+          return $http.get(api_url + "/timelines/" + timelineId);
         },
 
         createTimeline: function(name, description) {
@@ -53,11 +56,11 @@
             "description": description
           };
 
-          return $http.post($rootScope.api_url + "/timelines", data);
+          return $http.post(api_url + "/timelines", data);
         },
 
         deleteTimeline: function(timelineId) {
-          return $http.delete($rootScope.api_url + "/timelines/" + timelineId);
+          return $http.delete(api_url + "/timelines/" + timelineId);
         },
 
         getCards: function(timelineId, count, direction, query) {
@@ -75,70 +78,55 @@
             endpoint += "$q=" + query
           }
 
-          return $http.get($rootScope.api_url + endpoint);
+          return $http.get(api_url + endpoint);
 
         },
 
         createCard: function(timelineId, cardObj) {
-          return $http.post($rootScope.api_url + "/timelines/" + timelineId + "/cards", cardObj);
+          return $http.post(api_url + "/timelines/" + timelineId + "/cards", cardObj);
         },
 
         getCard: function(timelineId, cardId) {
-          return $http.get($rootScope.api_url + "/timelines/" + timelineId + "/cards/" + cardId);
+          return $http.get(api_url + "/timelines/" + timelineId + "/cards/" + cardId);
         },
 
         updateCard: function(timelineId, cardId, cardObj) {
-          return $http.patch($rootScope.api_url + "/timelines/" + timelineId + "/cards/"  + cardId, cardObj);
+          return $http.patch(api_url + "/timelines/" + timelineId + "/cards/"  + cardId, cardObj);
         },
 
         deleteCard: function(timelineId, cardId) {
-          return $http.delete($rootScope.api_url + "/timelines/" + timelineId + "/cards/"  + cardId);
+          return $http.delete(api_url + "/timelines/" + timelineId + "/cards/"  + cardId);
         },
 
         addAttachment: function(timelineId, cardId, attachmentObj) {
-          return $http.post($rootScope.api_url + "/timelines/" + timelineId + "/cards/"  + cardId + "/attachments", attachmentObj);
+          return $http.post(api_url + "/timelines/" + timelineId + "/cards/"  + cardId + "/attachments", attachmentObj);
         },
 
         deleteAttachment: function(timelineId, cardId, attachmentId) {
-          return $http.delete($rootScope.api_url + "/timelines/" + timelineId + "/cards/"  + cardId + "/attachments/" + attachmentId);
+          return $http.delete(api_url + "/timelines/" + timelineId + "/cards/"  + cardId + "/attachments/" + attachmentId);
         },
 
         getCardComments: function(timelineId, cardId) {
-          return $http.get($rootScope.api_url + "/timelines/" + timelineId + "/cards/" + cardId + "/comments");
+          return $http.get(api_url + "/timelines/" + timelineId + "/cards/" + cardId + "/comments");
         },
 
         postComment: function(timelineId, cardId, commentObj) {
-          return $http.post($rootScope.api_url + "/timelines/" + timelineId + "/cards/" + cardId + "/comments", commentObj);
+          return $http.post(api_url + "/timelines/" + timelineId + "/cards/" + cardId + "/comments", commentObj);
         },
 
         deleteComment: function(timelineId, cardId, commentId) {
-          return $http.delete($rootScope.api_url + "/timelines/" + timelineId + "/cards/" + cardId + "/comments/" + commentId);
+          return $http.delete(api_url + "/timelines/" + timelineId + "/cards/" + cardId + "/comments/" + commentId);
         },
 
         getEventsToken: function() {
-          return $http.get($rootScope.api_url + "/events")
+          return $http.get(api_url + "/events")
         }
 
       }
     })
     .config(function($httpProvider) {
-      $httpProvider.interceptors.push(function($rootScope, $q) {
+      $httpProvider.interceptors.push(function($q) {
         return {
-          "request": function(config) {
-
-            if ($rootScope.app_token) {
-              config.headers = {
-                "Authorization": "bearer " + $rootScope.app_token
-              }
-            } else {
-              config.headers = {
-                "X-Lifestreams-3scale-AppId": $rootScope.app_id,
-                "X-Lifestreams-3scale-AppKey": $rootScope.app_key
-              }
-            }
-
-            return config;
-          },
           "responseError": function(error) {
             throw new Error(error.status + ": " + error.data);
           }
